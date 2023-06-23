55ab53ec62a16361499b2a67a03a0c7dbedee62f
jordanfirthussf <jordan.firth@afacademy.af.edu>
2023-06-23 01:25:30 -0600

    lab 4 pieces

---
+++ lab_04_attitude/install_libraries.bat
@@ -0,0 +1,4 @@
+..\computer_environment\arduino-cli lib install "SparkFun 9DoF IMU Breakout - ICM 20948 - Arduino Library"@1.2.11
+..\computer_environment\arduino-cli lib install "TB9051FTGMotorCarrier"@1.0.2
+..\computer_environment\arduino-cli lib install "QuadratureEncoder"@1.0.0
+..\computer_environment\arduino-cli lib install "EnableInterrupt"@1.1.0
